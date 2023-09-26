aace5ff0654c312ae4e40cb42821022332ea57df
Abi <alybrynjohnson@gmail.com>
2023-09-26 18:12:10 -0400

    Day 1

--- 2015/1/aoc_2015_1.py
+++ 2015/1/aoc_2015_1.py
@@ -1,18 +1,42 @@
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_1 = [0, 0, 3, 3, 3, -1, -1, -3, -3]
 SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
 
+def move(ch):
+    if ch == "(":
+        return 1
+    else:
+        return -1
+
 def part1(parsed):
-    return 0
+    floors = []
+    for line in parsed:
+        floor = 0
+        for ch in list(line):
+            #print(f"{ch}\n")
+            floor += move(ch)
+        floors.append(floor)
+    return floors
 
 def part2(parsed):
-    return 0
+    ans = []
+    for line in parsed:
+        floor = 0
+        ch_list = list(line)
+        for i in range(len(ch_list)):
+            floor += move(ch_list[i])
+            if floor == -1:
+                ans.append(i+1)
+                break
+            if i == len(ch_list) - 1:
+                ans.append(-1)
+    return ans
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
