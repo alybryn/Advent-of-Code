377b4b5b9ae77f60401b38287b70f0320cedf65e
Abi <alybrynjohnson@gmail.com>
2023-09-27 11:20:02 -0400

    Day 2

--- 2015/2/aoc_2015_2.py
+++ 2015/2/aoc_2015_2.py
@@ -1,18 +1,44 @@
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
-SAMPLE_ANSWER_2 = None
+SAMPLE_ANSWER_1 = 101
+SAMPLE_ANSWER_2 = 48
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    # return a list of lists [[l,w,h]...]
+    ls = [line.split('x') for line in puzzle_input.split()]
+    ret = []
+    for l in ls:
+        ret.append((int(l[0]), int(l[1]), int(l[2])))
+    return ret
 
 def part1(parsed):
-    return 0
+    paper = []
+    for line in parsed:
+        # find the three sides
+        sides = [
+            line[0] *line[1],
+            line[1] *line[2],
+            line[0] *line[2]]
+        # find the min for slack
+        slack = min(sides)
+        paper.append(2*sum(sides)+slack)
+    # print(paper)
+    return sum(paper)
 
 def part2(parsed):
-    return 0
+    ribbon = []
+    for line in parsed:
+        #find the smallest face
+        faces = [line[0] + line[1],
+                 line[1] + line[2],
+                 line[0] + line[2]]
+        ribbon.append(min(faces) * 2)
+        #find the volume
+        ribbon.append(line[0] * line[1] * line[2])
+    # print(ribbon)
+    return sum(ribbon)
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
