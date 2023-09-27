1941649a239dd2d333b35483cf3fd4d39f68b3fc
Abi <alybrynjohnson@gmail.com>
2023-09-27 11:50:27 -0400

    Day 3 Done

--- 2015/3/aoc_2015_3.py
+++ 2015/3/aoc_2015_3.py
@@ -1,18 +1,52 @@
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_1 = 2
 SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    return list(puzzle_input)
+
+def move(c):
+    if c == '^':
+        return (1, 0)
+    elif c == 'v':
+        return (-1, 0)
+    elif c == '<':
+        return (0, -1)
+    else:
+        return (0, 1)
 
 def part1(parsed):
-    return 0
+    # print(parsed)
+    #     (ud, lr)
+    curr = (0, 0)
+    houses = {curr}
+    for c in parsed:
+        m = move(c)
+        curr = (curr[0] + m[0], curr[1] + m[1])
+        houses.add(curr)
+    # print(houses)
+    return len(houses)
 
 def part2(parsed):
-    return 0
+    # print(parsed)
+    santas_turn = True
+    robot = (0,0)
+    real = (0,0)
+    houses = {robot, real}
+    for c in parsed:
+        # print(f"santa {real} : robot {robot}")
+        m = move(c)
+        if santas_turn:
+            real = (real[0] + m[0], real[1] + m[1])
+            houses.add(real)
+        else:
+            robot = (robot[0] + m[0], robot[1] + m[1])
+            houses.add(robot)
+        santas_turn = not santas_turn
+    return len(houses)
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
