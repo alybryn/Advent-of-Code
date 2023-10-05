0b9adcb90ebb468e16b40fef14589c37b20686c3
Abi <alybrynjohnson@gmail.com>
2023-10-05 16:29:20 -0400

    D8:P1&2

--- 2015/8/aoc_2015_8.py
+++ 2015/8/aoc_2015_8.py
@@ -1,6 +1,9 @@
 import pathlib
 import sys
 
+# code: 23
+# mem:  11
+# diff: 12
 SAMPLE_ANSWER_1 = None
 SAMPLE_ANSWER_2 = None
 
@@ -8,11 +11,55 @@ def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
 
+def string_count(input):
+    length = 0
+    escape_mode = False
+    skip_counter = 0
+    # skip wrapping ""
+    for i in range(1, len(input)-1):
+        if escape_mode:
+            if input[i] in ['\"', '\\']:
+                escape_mode = False
+            else:
+                skip_counter += 1
+                if skip_counter == 3:
+                    skip_counter = 0
+                    escape_mode = False
+        else:
+            length += 1
+            if input[i] == '\\':
+                escape_mode = True
+    return length
+
+def encode_string(input):
+    #start with enclosing quotes
+    length = 2
+    for c in list(input):
+        if c in ['\"', '\\']:
+            length += 2
+        else:
+            # print(f"{c} requires no extra encoding")
+            length += 1
+    # print(f"{input} is encoded in {length} characters")
+    return length
+
+
 def part1(parsed):
-    return 0
+    code = 0
+    memory = 0
+    for l in parsed:
+        code += len(l)
+        memory += string_count(l)
+    return code - memory
 
 def part2(parsed):
-    return 0
+    code = 0
+    reencode = 0
+    for l in parsed:
+        code += len(l)
+        # print(encode_string(l))
+        reencode += encode_string(l)
+    return reencode - code
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
