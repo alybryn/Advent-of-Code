69a5af2d8546204d7922e1a7d5fd71c4e6b57b3b
Abi <alybrynjohnson@gmail.com>
2023-11-30 15:58:47 -0500

    milestone: parsing input into instructions

--- 2021/code/2.py
+++ 2021/code/2.py
@@ -7,33 +7,44 @@ SAMPLE_ANSWER_1 = 150
 SAMPLE_ANSWER_2 = 900
 
 def parse(puzzle_input):
+    ret = []
     # parse the input
     lines = [line for line in puzzle_input.split("\n")]
     
-    #matcher = re.compile(r"((up)|(down)|(forward)) [0-9]")
-    udf_matcher = re.compile(r"(up)|(down)|(forward)")
-    num_matcher = re.compile(r"[0-9]")
-    up_matcher = re.compile(r"up")
-    down_matcher = re.compile(r"down")
-    forward_matcher = re.compile(r"forward")
+    pattern = r"(?P<dir>(up)|(down)|(forward)) (?P<val>[0-9])"
     
     for line in lines:
-        print(udf_matcher.match(line))
-        print(num_matcher.
+        #print(udf_matcher.match(line))
+        if (match := re.match(pattern, line)):
+            inst_dict = match.groupdict()
+            ret.append(Instruction(inst_dict['dir'], int(inst_dict['val'])))
+        else:
+            print(f"uhoh, parse({line})")
+    
+    return ret
 
 
 class Instruction:
-    directions = ['up', 'down', 'forward']
     class Direction(Enum):
         UP = 0
         DOWN = 1
         FORWARD = 2
 
-    def __init__(self, dir: Direction, val: int) -> None:
-        self._dir = dir
+    def __init__(self, dir: str, val: int) -> None:
+        self._dir = self.Direction[dir.upper()]
         self._val = val
+    
+    @property
+    def dir(self):
+        return self._dir
+
+    @property
+    def val(self):
+        return self._val
 
 def part1(parsed):
+    for p in parsed:
+        print(f"Instruction: {p.dir} {p.val}")
     return 0
 
 def part2(parsed):
