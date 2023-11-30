3e7099e55052c3256b94f3f9623518cda87c3728
Abi <alybrynjohnson@gmail.com>
2023-11-30 15:21:28 -0500

    starting day 2, learn Class and Enum stuff

--- 2021/code/2.py
+++ 2021/code/2.py
@@ -1,12 +1,37 @@
-import pathlib
+from enum import Enum
+from pathlib import Path
+import re
 import sys
 
-SAMPLE_ANSWER_1 = None
-SAMPLE_ANSWER_2 = None
+SAMPLE_ANSWER_1 = 150
+SAMPLE_ANSWER_2 = 900
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    lines = [line for line in puzzle_input.split("\n")]
+    
+    #matcher = re.compile(r"((up)|(down)|(forward)) [0-9]")
+    udf_matcher = re.compile(r"(up)|(down)|(forward)")
+    num_matcher = re.compile(r"[a-z]* [0-9]")
+    up_matcher = re.compile(r"up")
+    down_matcher = re.compile(r"down")
+    forward_matcher = re.compile(r"forward")
+    
+    for line in lines:
+        print(udf_matcher.match(line))
+        print(num_matcher.match(line))
+
+
+class Instruction:
+    directions = ['up', 'down', 'forward']
+    class Direction(Enum):
+        UP = 0
+        DOWN = 1
+        FORWARD = 2
+
+    def __init__(self, dir: Direction, val: int) -> None:
+        self._dir = dir
+        self._val = val
 
 def part1(parsed):
     return 0
@@ -24,7 +49,7 @@ def solve(puzzle_input):
 if __name__ == "__main__":
     for path in sys.argv[1:]:
         print(f"{path}")
-        puzzle_input = pathlib.Path(path).read_text().strip()
+        puzzle_input = Path(path).read_text().strip()
 
         solutions = solve(puzzle_input)
         print("\n".join(str(solution) for solution in solutions))
