431030077bece48dd07b41ef346553114c1a42c1
Abi <alybrynjohnson@gmail.com>
2023-11-30 21:13:43 -0500

    started parsing input, switching to dict approach

--- 2021/code/4.py
+++ 2021/code/4.py
@@ -1,17 +1,64 @@
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
-SAMPLE_ANSWER_2 = None
+SAMPLE_ANSWER_1 = 4512
+SAMPLE_ANSWER_2 = 1924
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    calls, *rem = puzzle_input.split('\n\n')
+    calls = [int(c) for c in calls.split(',')]
+    boards = []
+    for section in rem:
+        boards.append(BingoBoard(section))
+    return (calls, boards)
 
-def part1(parsed):
+class BingoSpace():
+    def __init__(self, row, col):
+        self._row = row
+        self._col = col
+        self._marked = False
+    
+    @property
+    def row(self):
+        return self._row
+
+    @property
+    def col(self):
+        return self._col
+
+    @property
+    def marked(self):
+        return self._marked
+
+    def mark(self):
+        self.marked = True
+
+class BingoBoard():
+    def __init__(self, input):
+        self._spaces = {}
+        rows = [[s for s in r.split] for r in input]
+        for r in range(5):
+            for c in range(5):
+                self._spaces.add(BingoSpace(r, c, rows[r][c]))
+
+    def print(self):
+        pr = ''
+        for r in range(5):
+            for c in range(5):
+                pr += self._spaces.
+        
+class BingoCompetition():
+    def __init__(self, calls, boards):
+        self._calls = calls
+        self._boards = boards
+
+def part1(calls_and_boards):
+    calls, boards = calls_and_boards
+    print(calls)
     return 0
 
-def part2(parsed):
+def part2(calls_and_boards):
     return 0
 
 def solve(puzzle_input):
