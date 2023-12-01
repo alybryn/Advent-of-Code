1f86f51032cd05fedf901d11921d3661e11e5b41
Abi <alybrynjohnson@gmail.com>
2023-11-30 23:43:17 -0500

    starting debugging. runs but answer is wrong

--- 2021/code/4.py
+++ 2021/code/4.py
@@ -18,32 +18,42 @@ class BingoBoard():
     def __init__(self, input):
         # {(r, c): n}
         self._spaces = {}
-        self._called = []
+        self._called = set()
         self._round = 0
         rows = [[s for s in re.findall(r'([0-9]+)[ ]?', r)] for r in input.split('\n')]
-        
         for r in range(5):
             for c in range(5):
-                self._spaces[(r, c)] = rows[r][c]
+                self._spaces[(r, c)] = int(rows[r][c])
                 
     def print(self):
         pr = ''
         for r in range(5):
             for c in range(5):
-                pr += self._spaces.get((r, c))
+                p = self._spaces.get((r, c))
+                if p in self._called:
+                    pr += f'!{p}!'
+                else:
+                    print(f'{[p]} is not in {self._called}')
+                    pr += str(p)
                 pr += ' '
             pr += '\n'
         print(pr)
 
-    def mark(self, called):
-        self._called.append(called)
+    def call(self, called):
+        self._called.add(called)
         self._round += 1
+        return (self._round, self._called)
     
     def is_winning(self):
         #check rows
         for r in range(5):
-            for c in range(5):
+            if self._spaces.get((r, 0)) in self._called and self._spaces.get((r, 1)) in self._called and self._spaces.get((r, 2)) in self._called and self._spaces.get((r, 3)) in self._called and self._spaces.get((r, 4)) in self._called:
+                return True
         #check columns
+        for c in range(5):
+            if self._spaces.get((0, c)) in self._called and self._spaces.get((1, c)) in self._called and self._spaces.get((2, c)) in self._called and self._spaces.get((3, c)) in self._called and self._spaces.get((4, c)) in self._called:
+                return True
+        return False
 
     def score(self):
         unmarked = 0
@@ -59,11 +69,21 @@ class BingoCompetition():
     def __init__(self, calls, boards):
         self._calls = calls
         self._boards = boards
+    
+    def run(self):
+        bingo = False
+        index = 0
+        while not bingo:
+            for board in self._boards:
+                board.call(self._calls[index])
+                if board.is_winning():
+                    return board.score()
+            index += 1
 
 def part1(calls_and_boards):
     calls, boards = calls_and_boards
-    boards[0].print()
-    return 0
+    our_competition = BingoCompetition(calls, boards)
+    return our_competition.run()
 
 def part2(calls_and_boards):
     return 0
