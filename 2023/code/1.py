3916c923ca43c5690914967bfccd4d7e23760e6b
Abi <alybrynjohnson@gmail.com>
2023-12-01 14:30:51 -0500

    back to non-regex part1 solution. solved nonbug

--- 2023/code/1.py
+++ 2023/code/1.py
@@ -2,8 +2,10 @@ import pathlib
 import re
 import sys
 
-SAMPLE_ANSWER_1 = None
-SAMPLE_ANSWER_2 = None
+### !    ANSWERS FOR 1b.txt ONLY     ! ###
+### ! 1a.txt WILL BREAK THIS PROGRAM ! ###
+SAMPLE_ANSWER_1 = 264
+SAMPLE_ANSWER_2 = 281
 
 DIGIT_STRINGS = ['0','1','2','3','4','5','6','7','8','9']
 
@@ -11,12 +13,13 @@ def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
 
-def get_digits(r, s):
-    return re.findall(r, s)
-    # return [d for d in s if d in DIGIT_STRINGS]
+def get_digits(s):
+    return [d for d in s if d in DIGIT_STRINGS]
 
+# takes a regex pattern with a capture group named 'd'
+# returns only the part that matches capture group 'd'
 def get_digits2(r, s):
-    return re.match(r, s).groupdict()
+    return re.match(r, s).groupdict().get('d')
 
 # def findall_digits(s):
 #     return re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', s)
@@ -40,8 +43,7 @@ def s_to_n(s):
 def part1(parsed):
     ret = 0
     for line in parsed:
-        # should still use findall?
-        digits = get_digits(r'\d', line)
+        digits = get_digits(line)
         ret += int(digits[0] + digits[-1])
     return ret
 
@@ -49,14 +51,12 @@ def part2(parsed):
     ret = 0
     i = 0
     for line in parsed:
-        digits = get_digits(r'\d|one|two|three|four|five|six|seven|eight|nine', line)
-        back_digits = get_digits(r'\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line[::-1])
-        digit1 = get_digits2(r'^(.*?)(?P<d>one|two|three|four|five|six|seven|eight|nine|\d)', line)
-        if digit1.get('d') != digits[0]:
-            print(f"1: {line} {digit1} != {digits[0]}")
-        digit2 = get_digits2(r'(.*)(?P<d>one|two|three|four|five|six|seven|eight|nine|\d)(.*?$)', line)
-        if digit2.get('d') != back_digits[0][::-1]:
-            print(f"2: {digit2} != {back_digits[0][::-1]}")
+        ### COMPLEMENTS OTHER ARTIFACT ###
+        # digits = get_digits(r'\d|one|two|three|four|five|six|seven|eight|nine', line)
+        # back_digits = get_digits(r'\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line[::-1])
+
+
+        ### AN IMPORTANT ARTIFACT, DON'T ERASE ###
         # if (digits[-1] != back_digits[0][::-1]):
         #     print('________')
         #     print(line)
@@ -67,7 +67,13 @@ def part2(parsed):
         #     print(digits)
         #     print(back_digits[1][::-1])
         #print("-----" + line + " " + s_to_n(digits[0]) + s_to_n(back_digits[0][::-1]))
-        ret += int(s_to_n(digits[0]) + s_to_n(back_digits[0][::-1]))
+
+        patt_1 = r'^(.*?)(?P<d>one|two|three|four|five|six|seven|eight|nine|\d)'
+        patt_2 = r'(.*)(?P<d>one|two|three|four|five|six|seven|eight|nine|\d)(.*?$)'
+        first_digit = get_digits2(patt_1, line)
+        last_digit = get_digits2(patt_2, line)
+
+        ret += int(s_to_n(first_digit) + s_to_n(last_digit))
     return ret
 
 def solve(puzzle_input):
