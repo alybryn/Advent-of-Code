5a5f8799ed886d2c5afcf0c643e0504645e42bea
Abi <alybrynjohnson@gmail.com>
2023-12-02 17:07:55 -0500

    finish part 2 with minor efficencies

--- 2021/code/7.py
+++ 2021/code/7.py
@@ -8,26 +8,41 @@ def parse(puzzle_input):
     # parse the input
     return [int(line) for line in puzzle_input.split(',')]
 
-def part1(parsed):
-    lowest = min(parsed)
-    highest = max(parsed)
+def fuel_equation(diff):
+    ret = 0
+    for i in range(diff + 1):
+        ret += i
+    return ret
+
+def fuel_loop(crabs, part = 1):
+    lowest = min(crabs)
+    highest = max(crabs)
     fuel_economy = None
-    # for every position inclusive between crabs, move every crab to that position
+    # move each crab to each position between min and max
     for i in range(lowest, highest + 1):
         fuel = 0
-
-        for crab in parsed:
-            fuel += (abs(i - crab))
+        for crab in crabs:
+            diff = abs(i - crab)
+            if part == 2:
+                diff = fuel_equation(diff)
+            fuel += diff
+            if fuel_economy is not None and fuel > fuel_economy:
+                break
 
         if fuel_economy is None:
             fuel_economy = fuel
-        
+
         if fuel < fuel_economy:
             fuel_economy = fuel
     return fuel_economy
 
+def part1(parsed):
+    return fuel_loop(parsed)
+
 def part2(parsed):
-    return 0
+    # for i in range(4):
+    #     print(f"{i} is {fuel_equation(i)}")
+    return fuel_loop(parsed, 2)
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
