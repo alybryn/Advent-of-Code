6e425d7417250941ae10fe04da74a7716eeb41d1
Abi <alybrynjohnson@gmail.com>
2023-12-02 12:43:21 -0500

    played my little games

--- 2023/code/2.py
+++ 2023/code/2.py
@@ -54,7 +54,21 @@ def part1(parsed):
     return ret
 
 def part2(parsed):
-    return 0
+    # find the greatest number called of each color in each game
+    ret = 0
+    for k in parsed.keys():
+        minimum = {Color.RED: 0, Color.GREEN: 0, Color.BLUE: 0}
+        game = parsed.get(k)
+        for round in game:
+            for color in Color:
+                m = round.get(color, 0)
+                if m > minimum.get(color):
+                    minimum.update({color: m})
+            power = 1
+            for color in Color:
+                power = power * minimum.get(color)
+        ret += power
+    return ret
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
