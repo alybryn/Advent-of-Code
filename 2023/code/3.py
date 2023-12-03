bab13e060e82b07c14284ec6e4fdad95d7baa81b
Abi <alybrynjohnson@gmail.com>
2023-12-03 10:19:26 -0500

    setting up data structures

--- 2023/code/3.py
+++ 2023/code/3.py
@@ -8,6 +8,26 @@ def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
 
+# def xy_neighbors(pair):
+#     # diagonals too
+#     matrix = [(-1,0), (1,0), (0,-1), (0,1), (-1,-1), (1,1), (-1,1), (1,-1)]
+#     return [(pair[0] + m[0], pair[1] + m[1]) for m in matrix]
+
+class Num():
+    # val: int, points: [(x,y),...]
+    def __init__(self, value, points):
+        self._value = value
+        self._points = points
+    
+    @property
+    def neigbors(self):
+        ret = set()
+        # diagonals too
+        matrix = [(-1,0), (1,0), (0,-1), (0,1), (-1,-1), (1,1), (-1,1), (1,-1)]
+        for p in self._points:
+            ret.add([(p[0] + m[0], p[1] + m[1]) for m in matrix])
+        return ret
+
 def part1(parsed):
     return 0
 
