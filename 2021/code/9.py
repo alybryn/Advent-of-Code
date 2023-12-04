553e13ef2c165d23e5d6ed4a0d9866ead463997a
Abi <alybrynjohnson@gmail.com>
2023-12-04 14:58:01 -0500

    wrote basin logic

--- 2021/code/9.py
+++ 2021/code/9.py
@@ -17,20 +17,46 @@ def adjacent(x, y):
     matrix = [(0,1), (0,-1), (1,0), (-1,0)]
     return [(x + m[0], y + m[1]) for m in matrix]
 
-def part1(parsed):
-    # find low points
+def define_basin(low_point, floor_map):
+    checked = set()
+    ret_update, checked_update = basin_neighbors(low_point, floor_map, checked)
+    return ret_update
+
+def basin_neighbors(point, floor_map, checked):
+    ret = set()
+    for n in adjacent(point):
+        if n not in checked:
+            checked.add(n)
+            ret_update, checked_update = basin_neighbors(n, floor_map, checked)
+            ret.update(ret_update)
+            checked.update(checked_update)
+            if floor_map.get(n, 9) != 9:
+                ret.add(n)
+    return (ret, checked)
+
+def find_low_points(floor_map):
     low_points = []
-    for k in parsed.keys():
-        height = parsed.get(k)
+    for k in floor_map.keys():
+        height = floor_map.get(k)
         lowest = True
         for xy in adjacent(k[0], k[1]):
-            if parsed.get(xy, 10) < height:
+            # if xy-point _higher_or_level_with_ k-point.
+            if floor_map.get(xy, 10) <= height:
                 lowest = False
         if lowest:
             low_points.append(k)
-    low_points_values = [parsed.get(l) for l in low_points]
+    for low in low_points:
+        print(f"{floor_map.get(low)} at {low}")
+    low_points_values = [floor_map.get(l) for l in low_points]
+
+def risk_level(n):
+    return n + 1
+
+def part1(parsed):
+    # find low points
+    low_points = find_low_points(parsed)
     # sum low points + len(lowpoints)
-    return sum(low_points_values) + len(low_points)
+    return sum([risk_level(l) for l in low_points_values])
 
 def part2(parsed):
     return 0
