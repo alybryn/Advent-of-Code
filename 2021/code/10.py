36639e24be2e66ff6dfeb634a374ef0fdc7cb0a3
Abi <alybrynjohnson@gmail.com>
2023-12-04 16:05:56 -0500

    part 1 done

--- 2021/code/10.py
+++ 2021/code/10.py
@@ -10,8 +10,38 @@ def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
 
+def find_corrupted_value(chunk, expectation):
+    found = '}'
+    return f'Expected {expectation} but found {found} instead'
+
+# def scan_line(line):
+#     bad = 0
+#     for i in range(len(line)):
+#         print((line[i+1:], line[i]))
+#     return char_scores.get(line[bad], 0)
+
+def scan_line(line):
+    expectations = []
+    pairs = {'(':')', '[':']', '{':'}', '<':'>'}
+    for i in range(len(line)):
+        if line[i] in ['(', '[', '{', '<']:
+            # creates an expection
+            expectations.append(pairs.get(line[i]))
+        # think about checking empty expections here #
+        elif line[i] == expectations[-1]:
+            expectations.pop()
+        # case line[i] is not expected, return char
+        else:
+            return line[i]
+    # case unfulfilled expections remain
+    if len(expectations) != 0:
+        return str(expectations)
+    
 def part1(parsed):
-    return 0
+    score = 0
+    for line in parsed:
+        score += char_scores.get(scan_line(line), 0)
+    return score
 
 def part2(parsed):
     return 0
