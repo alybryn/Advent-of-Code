18c7a898f03fb15d0930cd8ff353c78f068d7b50
Abi <alybrynjohnson@gmail.com>
2023-12-04 12:22:00 -0500

    complete part 2

--- 2023/code/4.py
+++ 2023/code/4.py
@@ -5,6 +5,9 @@ import sys
 SAMPLE_ANSWER_1 = None
 SAMPLE_ANSWER_2 = None
 
+wins_per_card = {}
+number_of_cards = {}
+
 def parse(puzzle_input):
     # parse the input
     wins = {}
@@ -19,26 +22,45 @@ def parse(puzzle_input):
         haves.update({card_number: [int(h) for h in re.split(r'\s+', having)]})
     return wins, haves
 
-def score_1(win, have):
+def score_card(win, have):
     score = 0
+    matches = 0
     for w in win:
         if w in have:
+            matches += 1
             if score == 0:
                 score = 1
             else:
                 score = score * 2
     # print(score)
-    return score
+    return score, matches
 
 def part1(parsed):
     winning, having = parsed[0], parsed[1]
     score = 0
     for k in winning.keys():
-        score += score_1(winning.get(k), having.get(k))
+        s, m = score_card(winning.get(k), having.get(k))
+        wins_per_card.update({k: m})
+        number_of_cards.update({k: 1})
+        score += s
     return score
 
 def part2(parsed):
-    return 0
+    total = 0
+    # print(wins_per_card)
+    # print(number_of_cards)
+    for k in wins_per_card.keys():
+        # add this card's number to total
+        add = number_of_cards.get(k)
+        total += add
+        # increase count of next .get(k) cards
+        # BY HOW MANY OF THIS CARD
+        increase = wins_per_card.get(k)
+        for i in range(k, k + increase):
+            # adjust up
+            number_of_cards.update({i+1: number_of_cards.get(i+1) + add})
+    # print(number_of_cards)
+    return total
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
