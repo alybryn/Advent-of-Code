05a605e4f14acd17d094f5ee1086618cb2e8a381
Abi <alybrynjohnson@gmail.com>
2023-12-05 10:17:04 -0500

    parsing input into seeds, [[maps]]

--- 2023/code/5.py
+++ 2023/code/5.py
@@ -1,4 +1,5 @@
 import pathlib
+import re
 import sys
 
 SAMPLE_ANSWER_1 = None
@@ -7,14 +8,30 @@ SAMPLE_ANSWER_2 = None
 def parse(puzzle_input):
     # parse the input
     lines = puzzle_input.split('\n\n')
-    seeds = [int(l) for l in lines[0].split(' ')[1:]]
-    # seed -> fertilizer
-    # fertilizer -> water
-    # water -> light
-    # light -> temperature
-    # temperature -> humidity
-    # humidity -> location
-    return seeds
+    # seeds = [int(l) for l in lines[0].split(' ')[1:]]
+    # # seed -> fertilizer
+    # fertilizer_ints = [[int(i) for i in l.split(' ')] for l in lines[1].split('\n')[1:]]
+    # fertilizer = [AlmanacMap(i[0], i[1], i[2]) for i in [f for f in fertilizer_ints]]
+    # # fertilizer -> water
+    # lines[2]
+    # # water -> light
+    # lines[3]
+    # # light -> temperature
+    # lines[4]
+    # # temperature -> humidity
+    # lines[5]
+    # # humidity -> location
+    # lines[6]
+    seeds = []
+    maps = []
+    for line in lines:
+        if re.match(r'^seeds:', line):
+            seeds = [int(l) for l in line.split(' ')[1:]]
+        else:
+            ints = [[int(i) for i in l.split(' ')] for l in line.split('\n')[1:]]
+            maps.append([AlmanacMap(i[0], i[1], i[2]) for i in [f for f in ints]])
+
+    return seeds, maps
 
 class AlmanacMap():
     def __init__(self, destination_range_start, source_range_start, range_length) -> None:
@@ -28,6 +45,8 @@ class AlmanacMap():
             diff = input - self._srs
             return self._drs + diff
 
+    def __repr__(self) -> str:
+        return f'AlmanacMap: {self._drs}, {self._srs}, {self._rl}, (optionally) {self._sr_end}'
 
 class Almanac():
     def __init__(self, seeds, maps) -> None:
