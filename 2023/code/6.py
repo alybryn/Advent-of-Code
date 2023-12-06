c60686f24a350b821995c88266ec6bb6866c47e6
Abi <alybrynjohnson@gmail.com>
2023-12-06 09:24:39 -0500

    parts 1 and 2, needs optomized, but done. TADA

--- 2023/code/6.py
+++ 2023/code/6.py
@@ -1,18 +1,38 @@
 import pathlib
+import re
 import sys
 
 SAMPLE_ANSWER_1 = 288
-SAMPLE_ANSWER_2 = None
+SAMPLE_ANSWER_2 = 71503
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    return [line for line in puzzle_input.split('\n')]
+
+def better_times(boat_race):
+    ret = 0
+    race = boat_race[0]
+    record = boat_race[1]
+    for hold in range(race):
+        speed = hold
+        travel_time = race - hold
+        if record < speed * travel_time:
+            ret += 1
+    return ret
 
 def part1(parsed):
-    return 0
+    ret = 1
+    races = [int(r) for r in re.findall(r'\d+', parsed[0])]
+    record = [int(r) for r in re.findall(r'\d+', parsed[1])]
+    for boat_race in zip(races, record):
+        ret = ret * better_times(boat_race)
+    return ret
 
 def part2(parsed):
-    return 0
+    time = [r for r in re.findall(r'\d+', parsed[0])]
+    record = [r for r in re.findall(r'\d+', parsed[1])]
+    print([int(''.join(time)), int(''.join(record))])
+    return better_times((int(''.join(time)), int(''.join(record))))
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
