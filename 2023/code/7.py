b30902566dece780f7bc96737871230d91e3343b
Abi <alybrynjohnson@gmail.com>
2023-12-07 09:48:52 -0500

    parsing input as hand object

--- 2023/code/7.py
+++ 2023/code/7.py
@@ -7,10 +7,36 @@ SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    return [[Hand(l[0], int(l[1])) for l in line.split()] for line in puzzle_input.split('\n')]
+
+class HandType(int, Enum):
+    FIVE = 6
+    FOUR = 5
+    FULL = 4
+    THREE = 3
+    TWO = 2
+    ONE = 1
+    HIGH = 0
+
+class Hand():
+    def __init__(self, cards, bet) -> None:
+        self._cards = cards
+        self._type = HandType.FIVE
+        self._bet = bet
+
+    def __gt__(self, other):
+        # TODO
+        return True
+    
+    def winnings(self, rank):
+        return self._bet * rank
+    
+    def __str__(self) -> str:
+        return f'Hand with:\n\tCards: {self._cards}\n\tType: {self._type}\n\tBet: {self._bet}'
+        
 
 def part1(parsed):
-    return 0
+    return parsed
 
 def part2(parsed):
     return 0
