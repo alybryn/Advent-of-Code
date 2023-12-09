4f3c8b2597a1d841dc510fe15f65729ba6a824ee
Abi <alybrynjohnson@gmail.com>
2023-12-09 11:17:50 -0500

    get the differences down to all zeros

--- 2023/code/9.py
+++ 2023/code/9.py
@@ -8,14 +8,25 @@ def parse(puzzle_input):
     # parse the input
     return [[int(l) for l in line.split(' ')] for line in puzzle_input.split('\n')]
 
-def get_differenct(input):
+def get_difference(input):
     ret = []
     for i in range(len(input)-1):
         ret.append(input[i+1] - input[i])
     return ret
 
+def get_sequences(input):
+    result = []
+    for sequence in input:
+        this_result = [sequence[-1]]
+        # alt: while s.count(0) != len(s):
+        while False in [s == 0 for s in sequence]:
+            sequence = get_difference(sequence)
+            this_result.append(sequence[-1])
+        result.append(this_result)
+    return result
+
 def part1(parsed):
-    return parsed
+    return get_sequences(parsed)
 
 def part2(parsed):
     return 0
