47346b2fa3816e31cade27e739fd889c53fd4c14
Abi <alybrynjohnson@gmail.com>
2023-12-10 12:55:53 -0500

    remove unneeded vars

--- 2023/code/10.py
+++ 2023/code/10.py
@@ -26,10 +26,8 @@ class PipeMap():
     def __init__(self, input) -> None:
         self._locs = {}
         self._start = None
-        self._height = len(input)
-        self._width = len(input[0])
-        for r in range(self._height):
-            for c in range(self._width):
+        for r in range(len(input)):
+            for c in range(len(input[0])):
                 type = input[r][c]
                 self._locs.update({(c,r): type})
                 if type == 'S':
