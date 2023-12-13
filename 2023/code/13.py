d992e40e922503abb93ad4b2c319182be3197095
Abi <alybrynjohnson@gmail.com>
2023-12-13 14:43:44 +0000

    first stab at part 1 logic

--- 2023/code/13.py
+++ 2023/code/13.py
@@ -12,6 +12,20 @@ def parse(puzzle_input):
         patterns_flipped.append([''.join(list(f)) for f in zip(*[list(c) for c in pattern.split('\n')])])
     return [pattern.split('\n') for pattern in patterns], patterns_flipped
 
+def is_mirror(side1, side2):
+    for i in range(min(len(side1), len(side2))):
+        if side1[i] != side2[i]:
+            return False
+    return True
+
+def find_inflection(pattern):
+    for i in range(1, len(pattern)):
+        # pass sides to helper
+        print(pattern[:i])
+        print(pattern[i:][::-1])
+        if is_mirror(pattern[:i], pattern[i:][::-1]):
+            return i
+
 
 def part1(parsed):
     return 0
