5725399025f7dd0f0da7b35613f682161e7bb0e5
Abi <alybrynjohnson@gmail.com>
2023-12-15 15:01:09 +0000

    stubbed out Boxes class

--- 2023/code/15.py
+++ 2023/code/15.py
@@ -16,6 +16,44 @@ def hash(chunk):
         ret = ret%256
     return ret
 
+class Boxes():
+    def __init__(self) -> None:
+        self._boxes = {}
+
+    def process(self, instruction):
+        label, *lens = instruction.split(',')
+        box = hash(label)
+        if '-' in instruction:
+            # remove lens with label from box h
+            self.remove(box, label)
+        else: # '=' in chunk
+            self.place(box, label, lens[0])
+    
+    # remove the lens with the given label if it is present
+    # any other lenses should be moved forward
+    def remove(self, box, label):
+        unshelved = self._boxes.get(box, [])
+        if label in unshelved:
+            i = list
+
+    def place(self, box, label, lens):
+        unshelved = self._boxes.get(box, [])
+        if label
+
+    def focusing_power(self):
+        ret = 0
+        for k in self._boxes.keys():
+            for l in self._boxes.get(k):
+                pass
+
+    def __str__(self) -> str:
+        p = ''
+        for k in self._boxes.keys():
+            p += f'Box {k}: '
+            for l in self._boxes.get(k):
+                p += str(l)
+        return p
+
 def part1(parsed):
     ret = 0
     for chunk in parsed:
