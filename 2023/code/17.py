87e4607fb9783470dd609269b23fa6525988ce19
Abi <alybrynjohnson@gmail.com>
2023-12-17 17:05:07 +0000

    parsing input into a data structure

--- 2023/code/17.py
+++ 2023/code/17.py
@@ -1,3 +1,4 @@
+from collections import deque, namedtuple
 import pathlib
 import sys
 
@@ -6,7 +7,47 @@ SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    ret = {}
+    lines = [[int(l) for l in list(line)] for line in puzzle_input.split()]
+    for x in range(len(lines)):
+        for y in range(len(lines[0])):
+            ret.update({Block(x, y): lines[x][y]})
+    return CityMap(ret, len(lines), len(lines[0]))
+
+Bounds = namedtuple('Bounds', ['min_x', 'max_x', 'min_y', 'max_y'])
+
+class Block(namedtuple('Block', ['x', 'y'])):
+    def __repr__(self) -> str:
+        return f'({self.x},{self.y})'
+
+class CityMap():
+    def __init__(self, blocks, max_x, max_y, min_y=0, min_x=0) -> None:
+        self._heat_map = blocks
+        self._dimensions = Bounds(min_x, max_x, min_y, max_y)
+        self._edges = {}
+
+        for x in range(min_x, max_x):
+            for y in range(min_y, max_y):
+                self._edges.update({Block(x, y):[Block(x  ,y-1),
+                Block(x  ,y+1),
+                Block(x-1,y  ),
+                Block(x+1,y  )]})
+
+    def neighbors(self, block):
+        return self._edges.get(block)
+
+    def cost(self, block):
+        # this is where the ring of fire goes...
+        return self._heat_map.get(block, 1000)
+    
+    def __str__(self) -> str:
+        p = ''
+        for x in range(self._dimensions.min_x, self._dimensions.max_x):
+            for y in range(self._dimensions.min_y, self._dimensions.max_y):
+                p += str(self._heat_map.get((x, y)))
+            p += '\n'
+        return p
+
 
 def part1(parsed):
     return 0
