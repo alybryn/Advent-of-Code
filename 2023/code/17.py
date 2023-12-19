1094a2d16f3836120d2582b71b8088fb8ad66bcc
Abi <alybrynjohnson@gmail.com>
2023-12-19 13:42:45 +0000

    things uncommitted at midnight 2 nights ago *shrug*

--- 2023/code/17.py
+++ 2023/code/17.py
@@ -31,21 +31,31 @@ class CityMap():
 
         for x in range(min_x, max_x):
             for y in range(min_y, max_y):
-                self._edges.update({Block(x, y):[Block(x  ,y-1),
-                Block(x  ,y+1),
-                Block(x-1,y  ),
-                Block(x+1,y  )]})
+                # self._edges.update({Block(x, y):[Block(x  ,y-1),
+                #                                  Block(x  ,y+1),
+                #                                  Block(x-1,y  ),
+                #                                  Block(x+1,y  )
+                #                                  ]})
+                self._edges.update({Block(x,y):[(Block(x  ,y-1),'<'),
+                                                (Block(x  ,y+1),'>'),
+                                                (Block(x-1,y  ),'^'),
+                                                (Block(x+1,y  ),'v')
+                                                ]})
+
+    @property
+    def start(self):
+        return Block(self._dimensions.min_x, self._dimensions.min_y)
+
+    @property
+    def end(self):
+        return Block(self._dimensions.max_x - 1, self._dimensions.max_y - 1)
 
     def neighbors(self, block):
         return self._edges.get(block)
 
     def cost(self, from_blocks, to_block):
-        admissable = False
-        for block in from_blocks:
-            if block.x != to_block.x or block.y != to_block.y:
-                admissable = True
         # this is where the ring of fire goes...
-        return self._heat_map.get(to_block, INF_COST) if admissable else INF_COST
+        return self._heat_map.get(to_block, INF_COST)
     
     def __str__(self) -> str:
         p = ''
@@ -62,29 +72,46 @@ def a_star(graph, start, goal):
     frontier.put((0,start))
     came_from = {}
     cost_so_far = {}
+    directions = {}
     came_from[start] = None
     cost_so_far[start] = 0
+    directions[start] = None
 
-    while not frontier.empty:
-        current = frontier.get()
-
+    while frontier.qsize() != 0:
+        pri, current = frontier.get()
+        print(f'({current})')
+        # print_visited(came_from)
+        # print(cost_so_far[current])
         if current == goal:
-            break
+            comprehensive_print(came_from, cost_so_far, current)
+            return cost_so_far[current]
         
-        # create list of last three blocks
+        # create list of last three dirs
         prev1 = came_from.get(current)
         prev2 = came_from.get(prev1)
-        prev = [current, prev1, prev2]
+        prev3 = came_from.get(prev2)
+        prev = [directions.get(current), directions.get(prev1), directions.get(prev2)]#, directions.get(prev3)]
+        print(current, prev1, prev2)#, prev3)
+        print(prev)
 
-        for next in graph.neighbors(current):
+        # print(f'neighbors: {graph.neighbors(current)}')
+        # next is ((x,y), '[<|>|^|v]')
+        for next, dir in graph.neighbors(current):
+            print(next)
             if next == came_from[current]:
                 continue
+
+            if prev.count(dir) == 3:
+                continue
+
             new_cost = cost_so_far[current] + graph.cost(prev, next)
+            print(new_cost)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
+                priority = new_cost #+ heuristic(goal, next)
+                frontier.put((priority, next))
                 came_from[next] = current
+                directions[next] = dir
 
 def heuristic(goal: Block, next: Block):
     return manhattan_distance(goal, next)
@@ -92,8 +119,32 @@ def heuristic(goal: Block, next: Block):
 def manhattan_distance(a: Block, b: Block):
     return abs(a.x - b.x) + abs(a.y - b.y)
 
+def print_path(graph, last):
+    print('printing path')
+    while graph[last]:
+        print(graph[last])
+        last = graph[last]
+
+def print_visited(came_from):
+    p = ''
+    for x in range(12):
+        for y in range(12):
+            p += '#' if (x,y) in came_from else '.'
+        p += '\n'
+    print(p)
+
+def at_what_cost(cost_so_far):
+    for x in range(12):
+        for y in range(12):
+            print(f'({x},{y}): {cost_so_far.get((x,y),0)}')
+
+def comprehensive_print(came_from, cost_so_far, last):
+    while came_from[last]:
+        print(f'{came_from[last]} -> {last} costs {cost_so_far[last]}')
+        last = came_from[last]
+
 def part1(parsed):
-    return 0
+    return a_star(parsed, parsed.start, parsed.end)
 
 def part2(parsed):
     return 0
