017c0db7e50de1dff97c333f4fad9290ab77b6d9
Abi <alybrynjohnson@gmail.com>
2023-12-20 15:35:44 +0000

    Broadcaster obj

--- 2023/code/20.py
+++ 2023/code/20.py
@@ -14,6 +14,27 @@ class Pulse(namedtuple('Pulse',['signal','destination', 'sender'])):
     def __repr__(self) -> str:
         return f'{self.signal.name} -> {self.destination}'
 
+class Broadcaster():
+    def __init__(self, destinations, modules) -> None:
+        self._name = 'broadcast'
+        self._destinations = destinations
+        self._all_modules = modules
+
+    def push_button(self):
+        count = 0
+        sending = []
+        for destination in self._destinations:
+            sending.append(Pulse(Signal.LOW,destination, self._name))
+        while sending:
+            count += len(sending)
+            to_send_on = []
+            for pulse in sending:
+                to_send_on.extend(self._all_modules.get(pulse.destination).receive(pulse))
+            sending = to_send_on
+        return count
+    
+    def __repr__(self) -> str:
+        return f'Broadcasting to {self._destinations} knowing about:\n' + '\n'.join([str(m) for m in self._all_modules.values()])
 def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
