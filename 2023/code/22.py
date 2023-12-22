b383c141f4a769e3f1593aff22833e69a2adb46e
Abi <alybrynjohnson@gmail.com>
2023-12-22 17:30:24 +0000

    settle bricks down on each other

--- 2023/code/22.py
+++ 2023/code/22.py
@@ -23,9 +23,88 @@ class SandBrick():
     def lowest(self):
         return min(self._point1.z,self._point2.z)
     
+    @property
+    def range(self):
+        return range(self._range_min, self._range_max)
+    
+    @property
+    def _range_min(self):
+        if self._orientation == Orientation.X:
+            return min(self._point1.x,self._point2.x)
+        if self._orientation == Orientation.Y:
+            return min(self._point1.y,self._point2.y)
+        if self._orientation == Orientation.Z:
+            return min(self._point1.z,self._point2.z)
+
+    @property
+    def _range_max(self):
+        if self._orientation == Orientation.X:
+            return max(self._point1.x,self._point2.x)+1
+        if self._orientation == Orientation.Y:
+            return max(self._point1.y,self._point2.y)+1
+        if self._orientation == Orientation.Z:
+            return max(self._point1.z,self._point2.z)+1
+
+    def drop(self):
+        self._point1 = Point_3D(self._point1.x,self._point1.y,self._point1.z-1)
+        self._point2 = Point_3D(self._point2.x,self._point2.y,self._point2.z-1)
+
+    @property
+    def all_points(self):
+        ret = set()
+        match self._orientation:
+            case Orientation.X:
+                for x in self.range:
+                    ret.add(Point_3D(x,self._point1.y,self._point1.z))
+            case Orientation.Y:
+                for y in self.range:
+                    ret.add(Point_3D(self._point1.x,y,self._point1.z))
+            case Orientation.Z:
+                for z in self.range:
+                    ret.add(Point_3D(self._point1.x,self._point1.y,z))
+        return ret
+    
+    def down_neighbors(self):
+        ret = []
+        match self._orientation:
+            case Orientation.X:
+                for x in self.range:
+                    ret.append(Point_3D(x,self._point1.y,self._point1.z-1))
+            case Orientation.Y:
+                for y in self.range:
+                    ret.append(Point_3D(self._point1.x,y,self._point1.z-1))
+            case Orientation.Z:
+                ret.append(Point_3D(self._point1.x,self._point1.y,min(self._point1.z,self._point2.z)-1))
+        return ret
     
     def __repr__(self) -> str:
         return f'{self._point1.x},{self._point1.y},{self._point1.z}~{self._point2.x},{self._point2.y},{self._point2.z}'
+    
+class Brick_Stack():
+    def __init__(self) -> None:
+        self._bricks = {}
+        self._occupied = set()
+    
+    def set(self,brick):
+        while brick.lowest > 1:
+            for neighbor in brick.down_neighbors():
+                if neighbor in self._occupied:
+                    # done moving
+                    self._occupied.update(brick.all_points)
+                    level = self._bricks.get(brick.lowest,[])
+                    level.append(brick)
+                    self._bricks[brick.lowest] = level
+                    return
+            # move down:
+            brick.drop()
+        # settles on ground
+        self._occupied.update(brick.all_points)
+        level = self._bricks.get(brick.lowest,[])
+        level.append(brick)
+        self._bricks[brick.lowest] = level
+
+    def __repr__(self) -> str:
+        return str(self._bricks)
 
 def parse(puzzle_input):
     # parse the input
@@ -38,6 +117,14 @@ def parse(puzzle_input):
         level.append(brick)
         ret[brick.lowest]=level
     return ret
+
+def settle_on_down(snap_shot):
+    settled = Brick_Stack()
+    for z in sorted(snap_shot.keys()):
+        print(z)
+        for brick in snap_shot.get(z):
+            settled.set(brick)
+    return settled
 
 def part1(parsed):
     return 0
