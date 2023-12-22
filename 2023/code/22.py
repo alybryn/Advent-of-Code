f091e458da998d3a0cf751e0ef6573e6ed7b7639
Abi <alybrynjohnson@gmail.com>
2023-12-22 15:55:47 +0000

    parse input to produce SandBrick

--- 2023/code/22.py
+++ 2023/code/22.py
@@ -1,12 +1,46 @@
+from collections import namedtuple
+from enum import Enum
 import pathlib
 import sys
 
 SAMPLE_ANSWER_1 = 5
 SAMPLE_ANSWER_2 = None
 
+Point_3D = namedtuple('Point_3D', ['x','y','z'])
+
+class Orientation(int,Enum):
+    X = 0
+    Y = 1
+    Z = 2
+
+class SandBrick():
+    # def __init__(self,p1,p2) -> None:
+    #     self._point1 = p1
+    #     self._point2 = p2
+    #     self._orientation = Orientation.X if self._point1.x != self._point2.x else Orientation.Y if self._point1.y != self._point2.y else Orientation.Z
+
+    def __init__(self,coords) -> None:
+        self._point1 = Point_3D(coords[0][0],coords[0][1],coords[0][2])
+        self._point2 = Point_3D(coords[1][0],coords[1][1],coords[1][2])
+        self._orientation = Orientation.X if self._point1.x != self._point2.x else Orientation.Y if self._point1.y != self._point2.y else Orientation.Z
+
+    @property
+    def lowest(self):
+        return min(self._point1.z,self._point2.z)
+    
+    
+    def __repr__(self) -> str:
+        return f'{self._point1.x},{self._point1.y},{self._point1.z}~{self._point2.x},{self._point2.y},{self._point2.z}'
+
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    lines = puzzle_input.split()
+    # dictionary of bricks with lowest point z {z:[SandBrick]}
+    ret = []
+    for line in lines:
+        brick = SandBrick([[int(i) for i in part.split(',')] for part in line.split('~')])
+        ret.append(brick)
+    return ret
 
 def part1(parsed):
     return 0
