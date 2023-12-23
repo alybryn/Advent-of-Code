cc40b59f3c6e7e2ea187b241f492acd7e536db8f
alybryn <alybryn@users.noreply.github.com>
2023-12-23 00:15:04 -0500

    Sample ans, some parsing

--- 2023/code/23.py
+++ 2023/code/23.py
@@ -1,15 +1,32 @@
+from collections import namedtuple
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_1 = 94
 SAMPLE_ANSWER_2 = None
+
+class Path(namedtuple('Path',['x','y','slope'])):
+    def __repr__(self):
+        return f'{self.x},{self.y}:{self.slope}'
+
+    def neighbors(self):
+        pass
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    lines =  puzzle_input.split()
+    start = None
+    end = None
+    for x in range(len(lines)):
+        for y in range(len(lines[0])):
+            if x == 0 and lines[x][y]=='.':
+                start = Path(x,y,lines[x][y])
+            if x == len(lines)-1 and lines[x][y]=='.':
+                end = Path(x,y,lines[x][y])
+            Path(x,y,lines[x][y])
 
 def part1(parsed):
-    return 0
+    return parsed
 
 def part2(parsed):
     return 0
