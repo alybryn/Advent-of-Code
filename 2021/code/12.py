a27b95ab9a6faabf3bb40465371bff523cee070b
Abi <alybrynjohnson@gmail.com>
2024-11-30 16:45:10 -0500

    solved part 1

--- 2021/code/12.py
+++ 2021/code/12.py
@@ -6,10 +6,40 @@ SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    ret = {}
+    for line in puzzle_input.split():
+        a, b = line.split("-")
+        up = ret.get(a,[])
+        up.append(b)
+        ret.update({a:up})
+        up = ret.get(b,[])
+        up.append(a)
+        ret.update({b:up})
+    return ret
+
+def findPath(path, system):
+    ret = []
+    for step in lookAhead(path, system):
+        if step != 'end':
+            ret+=(findPath(traverse(path,step),system))
+        else:
+            ret.append(str(f"{path},{step}"))
+    return ret
+
+def lookAhead(path, system):
+    ret = []
+    # print(f"{path}:{path.split(',')[-1]}")
+    for s in system.get(path.split(',')[-1]):
+        if s.isupper() or str(path).find(s) == -1:
+            ret.append(s)
+    return ret
+
+def traverse(path, step):
+    return str(f"{path},{step}")
 
 def part1(parsed):
-    return 0
+    # print(parsed)
+    return len(findPath('start',parsed))
 
 def part2(parsed):
     return 0
