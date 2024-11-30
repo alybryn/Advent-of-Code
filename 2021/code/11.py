22251913d188aba2eab4d2022360e399012421c7
Abi <alybrynjohnson@gmail.com>
2024-11-30 14:28:11 -0500

    parsed input, adjacency tested

--- 2021/code/11.py
+++ 2021/code/11.py
@@ -4,11 +4,37 @@ import sys
 SAMPLE_ANSWER_1 = None
 SAMPLE_ANSWER_2 = None
 
+# 100 octopuses in a 10x10 grid
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    return [[l for l in line] for line in puzzle_input.split()]
+
+def adjacentcy(x, y):
+    ret = []
+    for i in [-1,0, 1]:
+        for j in [-1,0,1]:
+            ret.append([x+i,y+j])
+    return [r for r in ret if 0 <= r[0] < 10 and 0 <=r[1] < 10]
+
+def energize(grid):
+    for i in range(0,10):
+        for j in range(0,10):
+            grid[i][j] = grid[i][j] + 1
+
+def flash(grid):
+    flashed = True
+    while flashed:
+        flashed = False
+        for i in range(1,10):
+            for j in range(1,10):
+                if grid[i][j] >= 9:
+                    flashed = True
+                    for a in adjacentcy(grid[i][j]):
+                        grid[a[0]][a[1]] = grid[a[0]][a[1]] + 1
 
 def part1(parsed):
+    print(parsed)
+    print(adjacentcy(9,9))
     return 0
 
 def part2(parsed):
