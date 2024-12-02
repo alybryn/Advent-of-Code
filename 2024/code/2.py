ac23511c9f881eb3e8efc2b7b32954eb6f7cff00
alybryn <alybryn@users.noreply.github.com>
2024-12-02 00:39:31 -0500

    Update 2.py, first pass part 1 , day 2

--- 2024/code/2.py
+++ 2024/code/2.py
@@ -8,9 +8,24 @@ def parse(puzzle_input):
     # parse the input
     return [line for line in puzzle_input.split()]
 
+def reportReport(report):
+    report = [int(r) for r in report.split(' ')]
+    rr = set()
+    for i in range(0, len(report)-1):
+        rr.add(report[i] - report[i+1])
+    return rr
+
+def isSafe(report):
+    rr = reportReport(report)
+    return rr <= {-1,-2,-3} or rr <= {1,2,3}
+
 def part1(parsed):
     print(parsed)
-    return parsed
+    c=0
+    for report in parsed:
+        if isSafe(report):
+            c += 1
+    return c
 
 def part2(parsed):
     return 0
