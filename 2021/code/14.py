0ae1bedd035657ef15f95e71c83b93c414914a2c
Abi <alybrynjohnson@gmail.com>
2024-12-10 14:34:00 -0500

    attempting recursive solution, currently over counting initial case

--- 2021/code/14.py
+++ 2021/code/14.py
@@ -34,8 +34,39 @@ def most_minus_less(template):
             min = temp
     return max - min
 
+def polymerize(rules, start, rep):
+    ret = {}
+    for i in range(0,len(start)-1):
+        print(f'counting: {start[i]}{start[i+1]}')
+        ret = dict_combine(ret, count_letters(rules, start[i], start[i+1],rep))
+        print(ret)
+    return most_minus_least(ret)
+
+def count_letters(rules, a, b, rep):
+    if rep == 0:
+        if a == b:
+            return {a:2}
+        return {a:1, b:1}
+    c = rules.get(f'{a}{b}')
+    if not c:
+        if a == b:
+            return {a:2}
+        return {a:1, b:1}
+    return dict_combine(count_letters(rules,a,c,rep-1),count_letters(rules,c,b,rep-1))
+
+def dict_combine(a, b):
+    keys = set(a.keys()).union(b.keys())
+    ret = {}
+    for key in keys:
+        ret.update({key: a.get(key,0) + b.get(key,0)})
+    return ret
+
+def most_minus_least(letter_dict):
+    return max(letter_dict.values()) - min(letter_dict.values())
+
 def part1(parsed):
     template, rules = parsed
+    return polymerize(rules, template, 0)
     for i in range(0, 10):
         # print(template)
         # print( '-------------')
@@ -43,10 +74,11 @@ def part1(parsed):
     return most_minus_less(template)
 
 def part2(parsed):
-    template, rules = parsed
-    for i in range(0, 40):
-        template = step(template, rules)
-    return most_minus_less(template)
+    # template, rules = parsed
+    # for i in range(0, 40):
+    #     template = step(template, rules)
+    # return most_minus_less(template)
+    return 0
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
