965e465a150711a618bf3a4efd4cdbd7d821edb9
Abi <alybrynjohnson@gmail.com>
2024-12-12 11:43:21 -0500

    complete overhaul to untangle plots with same crop

--- 2024/code/12.py
+++ 2024/code/12.py
@@ -21,21 +21,42 @@ SAMPLE_ANSWER_2 = None
 def parse(puzzle_input):
     # parse the input
     lines = [[l for l in line] for line in puzzle_input.splitlines()]
-    # make a dict. name:Plot
-    field = {}
-    for i in range(0, len(lines)):
-        for j in range(0, len(lines[0])):
-            if lines[i][j] not in field:
-                field[lines[i][j]] = Plot()
-            field[lines[i][j]].add((i,j))
+
+    field = []
+    plotted = set()
+    bounds = (len(lines)), len(lines[0])
+    for i in range(0, bounds[0]):
+        for j in range(0, bounds[1]):
+            if (i,j) not in plotted:
+                plot = travel_plot(lines,bounds,lines[i][j],(i,j))
+                # add to visited
+                plotted = plotted.union(plot.locs)
+                field.append(plot)
     return field
 
-def adj(loc):
+def travel_plot(lines,bounds, name, start):
+    visited = {start}
+    plot = Plot(name)
+    plot.add(start)
+    to_search = [start]
+    while to_search:
+        next = to_search.pop()
+        for adj in adjacent(next):
+            if adj not in visited:
+                if 0 <= adj[0] < bounds[0] and 0 <= adj[1] < bounds[1]:
+                    visited.add(adj)
+                    if lines[adj[0]][adj[1]] == name:
+                        plot.add(adj)
+                        to_search.append(adj)
+    return plot
+
+def adjacent(loc):
     vectors = [(0,1),(0,-1),(1,0),(-1,0)]
     return [(loc[0]+v[0],loc[1] + v[1]) for v in vectors]
 
 class Plot():
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
         self._locs = set()
 
     def add(self, loc):
@@ -45,18 +66,16 @@ class Plot():
         return len(self._locs)
 
     def perimeter(self):
-        for loc in self._locs:
-            for adj in adj(loc):
-                if adj in self._locs: 
-                    pass
-        return sum([1 for adj in adj(loc) for loc in self._locs if adj not in self._locs])
-
+        return sum([sum([1 for adj in adjacent(loc)if adj not in self._locs]) for loc in self._locs])
+    
+    @property
+    def locs(self):
+        return self._locs
 
 def part1(parsed):
-    print(parsed)
+    # print(parsed)
     ret = 0
-    for name, plot in parsed.items():
-        print(f'fields growing {name} in plots {plot._locs} with area {plot.area()} and perimeter {plot.perimeter()}')
+    for plot in parsed:
         ret += plot.area() * plot.perimeter()
     return ret
 
