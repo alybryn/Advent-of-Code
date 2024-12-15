83b585747b0fc64304d977cebb2bfbdb380fd53c
alybryn <alybryn@users.noreply.github.com>
2024-12-15 00:37:45 -0500

    two sample inputs, changed some things around so the program works in the dev container, character constants, parse input, State class with a repr(), part1 logic started

--- 2024/code/15.py
+++ 2024/code/15.py
@@ -1,13 +1,14 @@
 DAY = 15
 
-START = f'/home/abi/Documents/programming/Advent of Code/2024'
+START = f'/workspace/Advent of Code/2024'
 SAMPLE_PATH = f'{START}/sample/{DAY}.txt'
+SAMPLE_PATH_A = f'{START}/sample/{DAY}a.txt'
 DATA_PATH = f'{START}/data/{DAY}.txt'
 
 ONLY_ARGS = []
-ONLY_SAMPLE = [SAMPLE_PATH]
+ONLY_SAMPLE = [SAMPLE_PATH,SAMPLE_PATH_A]
 ONLY_DATA = [DATA_PATH]
-ALL = [SAMPLE_PATH, DATA_PATH]
+ALL = ONLY_SAMPLE + ONLY_DATA
 
 RUN = ONLY_SAMPLE
 
@@ -16,16 +17,60 @@ RUN = ONLY_SAMPLE
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_1 = 10092, 2028
 SAMPLE_ANSWER_2 = None
+
+ROBOT = '@'
+WALL = '#'
+BOX = 'O'
+
+UP = '^'
+DOWN = 'v'
+LEFT = '<'
+RIGHT = '>'
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    state, moves = puzzle_input.split('\n\n')
+    state_dict = {WALL:set(), BOX:set(), ROBOT:(0,0)}
+    moves = moves.replace('\n', '')
+    state = [s for s in state.splitlines()]
+    for i in range(0, len(state)):
+        for j in range(0,len(state[0])):
+            if state[i][j] in state:
+                state_dict[state[i][j]].add((i,j))
+    return State(state_dict,(len(state),len(state[0]))), moves
+
+class State():
+    def __init__(self, state, bounds):
+        self._boxes = state[BOX]
+        self._walls = state[BOX]
+        self._robot = state[ROBOT]
+        self._bounds = bounds
+
+    def move(self, instruction):
+        pass
+
+    def __repr__(self):
+        ret = ''
+        for j in range(0,self._bounds[1]):
+            for i in range(0, self._bounds[0]):
+                if (i,j) in self._boxes:
+                    ret += 'O'
+                elif (i,j) in self._walls:
+                    ret += '#'
+                elif (i,j) in self._robot:
+                    ret += '@'
+                else:
+                    ret += '.'
+            ret += '\n'
 
 def part1(parsed):
     print(parsed)
-    return parsed
+    state, moves = parsed
+    for move in moves:
+        state.move(move)
+    return state
 
 def part2(parsed):
     return 0
