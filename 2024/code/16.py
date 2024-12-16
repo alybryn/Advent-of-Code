1f3d24f742260b7dc22a567ab9cf6448268f480f
alybryn <alybryn@users.noreply.github.com>
2024-12-16 00:23:45 -0500

    two sample data, sample ans, diection enum, parsing input using enumerate, todo: dfs/bfs *shrug*

--- 2024/code/16.py
+++ 2024/code/16.py
@@ -1,11 +1,12 @@
 DAY = 16
 
-START = f'/home/abi/Documents/programming/Advent of Code/2024'
+START = f'/workspaces/Advent of Code/2024'
 SAMPLE_PATH = f'{START}/sample/{DAY}.txt'
+SAMPLE_PATH_A = f'{START}/sample/{DAY}a.txt'
 DATA_PATH = f'{START}/data/{DAY}.txt'
 
 ONLY_ARGS = []
-ONLY_SAMPLE = [SAMPLE_PATH]
+ONLY_SAMPLE = [SAMPLE_PATH_A, SAMPLE_PATH]
 ONLY_DATA = [DATA_PATH]
 ALL = [SAMPLE_PATH, DATA_PATH]
 
@@ -13,19 +14,37 @@ RUN = ONLY_SAMPLE
 
 # --------------------------------
 
+from enum import Enum
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_1 = 7036, 11048
 SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    return set([(i,j) for i, line in enumerate(puzzle_input.splitlines()) for j,l in enumerate(line()) if l == '#'])
+
+class Direction(Enum):
+    N = ( 0,-1)
+    E = ( 1, 0)
+    S = ( 0, 1)
+    W = (-1, 0)
+
+    def turn(self):
+        return {Direction.N:(Direction.W,Direction.E),
+        Direction.W:(Direction.S,Direction.N),
+        Direction.S:(Direction.E,Direction.W),
+        Direction.E:(Direction.N,Direction.S)}
+
+def dfs(walls, ptr, facing):
+    pass
 
 def part1(parsed):
     print(parsed)
-    return parsed
+    bounds = max(parsed)
+    s = (1, bounds[1]-1)
+    return bounds
 
 def part2(parsed):
     return 0
