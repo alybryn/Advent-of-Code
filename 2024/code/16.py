eacbbc8ed7332f467780bae4f56a3c81352e116c
Abi <alybrynjohnson@gmail.com>
2024-12-16 15:46:25 +0000

    costy search implemented, only returns minimum cost

--- 2024/code/16.py
+++ 2024/code/16.py
@@ -56,6 +56,35 @@ class PriorityQueue:
 
     def get(self): return heapq.heappop(self._elements)[1]
 
+def dfs(walls, start, end):
+    frontier = PriorityQueue()
+    frontier.put(start,0)
+    # dict[Loc, Optional[Loc]]
+    came_from = {}
+    # dict[Loc, float]
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        # only check loc, not dir
+        if current[0] == end:
+            return cost_so_far[current]
+
+        for next,cost in neighbors(current):
+            # check not in a wall
+            if next[0] in walls:
+                continue
+            new_cost = cost_so_far[current] + cost
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                frontier.put(next,new_cost)
+                came_from[next] = current
+    
+    return came_from, cost_so_far
+
 # returns ((location, direction), cost)
 def neighbors(loc_dir):
     loc,dir = loc_dir
