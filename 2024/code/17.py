c4e6512438c6924f1e9a3f08f1f2d126170b11be
alybryn <alybryn@users.noreply.github.com>
2024-12-17 01:17:29 -0500

    outline of Computer class penciled in, input parsed

--- 2024/code/17.py
+++ 2024/code/17.py
@@ -14,6 +14,7 @@ RUN = ONLY_SAMPLE
 # --------------------------------
 
 import pathlib
+import re
 import sys
 
 SAMPLE_ANSWER_1 = 4,6,3,5,6,3,5,2,1,0
@@ -21,33 +22,95 @@ SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    registers,instructions = puzzle_input.split('\n\nProgram: ')
+    a,b,c = re.findall(r'\d+',registers)
+    instructions = [int(i) for i in instructions.split(',')]
+    opcodes = [code for i,code in enumerate(instructions) if i%2==0]
+    operands = [op for i, op in enumerate(instructions) if i%2==1]
+    print(opcodes,operands)
+    return Computer(a,b,c,instructions)
 
 class Computer:
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c, instructions):
         self._register_a = a
         self._register_b = b
         self._register_c = c
+        self._instructions = instructions
+        self._ptr = 0
+    
+    def run(self):
+        while ptr < len(self. _instructions):
+            op = self.interpret_instruction(self._instructions[ptr])
+            op(self._instructions[ptr+1])
+            ptr += 2
 
-"""adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
+    def interpret_instruction(self, opcode):
+        match opcode:
+            case 0: return self.adv
+            case 1: return self.bxl
+            case 3: return self.jnz
+            case 4: return self.bxc
+            case 5: return self.out
+            case 6: return self.bdv
+            case 7: return self.cdv
+            case _: print(f'unknown opcode: {opcode}')
 
-The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+    def interpret_combo(self, operand):
+        match operand:
+            case 0: return 0
+            case 1: return 1
+            case 2: return 2
+            case 3: return 3
+            case 4: return self._register_a
+            case 5: return self._register_b
+            case 6: return self._register_c
+            case _: print(f'unknown combo operand: {operand}')
 
-The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+    # A division by 2^operand
+    # combo operand, truncate
+    # -> A
+    def adv(self, combo):
+        pass
 
-The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+    # The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+    def bxl(self, lit):
+        pass
 
-The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+    # The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+    def bst(self, combo):
+        pass
 
-The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+    # The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+    def jnz(self, lit):
+        pass
 
-The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
+    # The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+    def bxc(self,_):
+        pass
 
-The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)"""
+    # The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+    def out(self, combo):
+        pass
+
+    # The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
+    def bdv(self, combo):
+        pass
+
+    # the cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)"""
+    def cdv(self, combo):
+        pass
+
+    def __repr__(self):
+        ret = f'Register A: {self._register_a}\n'
+        ret += f'Register B: {self._register_b}\n'
+        ret += f'Register C: {self._register_c}\n\n'
+        ret += ','.join(self._instructions)
+        return ret
 
 def part1(parsed):
     print(parsed)
-    return parsed
+    output = parsed.run()
+    return output
 
 def part2(parsed):
     return 0
