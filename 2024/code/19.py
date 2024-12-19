aa81c2131f7e12be89036a5540b0a0d46129b4df
Abi <alybrynjohnson@gmail.com>
2024-12-19 17:33:29 +0000

    we're doing recursion AND regex now, because why not hop from scheme to scheme? anyway, (296) is also too low

--- 2024/code/19.py
+++ 2024/code/19.py
@@ -13,7 +13,9 @@ RUN = ONLY_SAMPLE
 
 # --------------------------------
 
+from collections import namedtuple
 import pathlib
+import re
 import sys
 
 SAMPLE_ANSWER_1 = 6
@@ -22,43 +24,41 @@ SAMPLE_ANSWER_2 = None
 def parse(puzzle_input):
     # parse the input
     towels, patterns = puzzle_input.split("\n\n")
-    towels = set([t for t in towels.split(", ")])
-    max_len =0
-    for t in towels:
-        if len(t) > max_len:
-            max_len = len(t)
-    patterns = patterns.splitlines()
-    return towels, patterns, max_len
+    towels = [Towel(t,re.compile(f'^{t}')) for t in towels.split(", ")]
+    designs = patterns.splitlines()
+    return towels, designs
+
+Towel = namedtuple('Towel', ['string','pattern'])
 
 DP = {'':1}
-def is_design_possible(towels, design, max_len):
+def is_design_possible(towels, design):
     if design not in DP:
         c = 0
-        matches = get_towels(towels,design[:max_len])
-        if matches:
-            for match in matches:
-                if is_design_possible(towels,design.removeprefix(match),max_len):
+        new_designs = remove_one_towel(towels,design)
+        if new_designs:
+            for n_d in new_designs:
+                if is_design_possible(towels,n_d):
                     c += 1
         DP[design] = c
     return DP[design]
 
 GT = {}
-def get_towels(towels, design):
+def remove_one_towel(towels, design):
     if design not in GT:
         ret = []
-        for sl in range(0,len(design)+1):
-            if design[:sl] in towels:
-                ret.append(design[:sl])
+        for towel in towels:
+                if re.match(towel.pattern,design):
+                    ret.append(design.removeprefix(towel.string))
         GT[design] = ret
     return GT[design]
 
 def part1(parsed):
     # print(parsed)
-    towels, designs, max_len = parsed
+    towels, designs = parsed
     c = 0
     for design in designs:
         print(design)
-        if is_design_possible(towels, design, max_len):
+        if is_design_possible(towels, design):
             c += 1
     return c
 
