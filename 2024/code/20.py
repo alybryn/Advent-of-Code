3f8be48889e8a9596efce5991ccb925ec9cef630
Abi <alybrynjohnson@gmail.com>
2024-12-20 14:52:53 +0000

    part2 first try, (1018101) is too high

--- 2024/code/20.py
+++ 2024/code/20.py
@@ -114,7 +114,14 @@ def part1(parsed):
     return c
 
 def part2(parsed):
-    return 0
+    time_saving_goal = 100
+    c = 0
+    for track in parsed.keys():
+        for neighbor in new_cheating_neighbors(track,20):
+            if neighbor in parsed:
+                if parsed[neighbor] - parsed[track] >= time_saving_goal:
+                    c += 1
+    return c
 
 def solve(puzzle_input):
     data = parse(puzzle_input)
