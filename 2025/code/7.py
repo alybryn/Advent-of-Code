8e4252fa1ec63eeb648723fd182b2f056e038ed7
Abi <alybrynjohnson@gmail.com>
2025-12-01 10:03:17 -0500

    populate 2025

---
+++ 2025/code/7.py
@@ -0,0 +1,52 @@
+DAY = 1
+YEAR = 2025
+
+START = f'/home/abi/Documents/programming/Advent of Code/{YEAR}'
+SAMPLE_PATH = f'{START}/sample/{DAY}.txt'
+DATA_PATH = f'{START}/data/{DAY}.txt'
+
+ONLY_ARGS = []
+ONLY_SAMPLE = [SAMPLE_PATH]
+ONLY_DATA = [DATA_PATH]
+ALL = [SAMPLE_PATH, DATA_PATH]
+
+RUN = ONLY_SAMPLE
+
+# --------------------------------
+
+import pathlib
+import sys
+
+SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_2 = None
+
+def parse(puzzle_input):
+    # parse the input
+    return [line for line in puzzle_input.split()]
+
+def part1(parsed):
+    print(parsed)
+    return 0
+
+def part2(parsed):
+    return 0
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+def run(path):
+    print(f'{path}')
+    puzzle_input = pathlib.Path(path).read_text().strip()
+
+    solutions = solve(puzzle_input)
+    print('\n'.join(str(solution) for solution in solutions))
+
+if __name__ == "__main__":
+    for path in RUN:
+        run(path)
+    for path in sys.argv[1:]:
+        run(path)
