f6dc6ac3f4468eab54fdbe764691eccb714533c4
Abi <alybrynjohnson@gmail.com>
2025-12-01 15:41:10 +0000

    finished day 1 part 1, day 1 sample data

--- 2025/code/1.py
+++ 2025/code/1.py
@@ -1,7 +1,7 @@
 DAY = 1
 YEAR = 2025
 
-START = f'/home/abi/Documents/programming/Advent of Code/{YEAR}'
+START = f'/workspaces/Advent of Code/{YEAR}'
 SAMPLE_PATH = f'{START}/sample/{DAY}.txt'
 DATA_PATH = f'{START}/data/{DAY}.txt'
 
@@ -17,16 +17,29 @@ RUN = ONLY_SAMPLE
 import pathlib
 import sys
 
-SAMPLE_ANSWER_1 = None
+SAMPLE_ANSWER_1 = 3
 SAMPLE_ANSWER_2 = None
 
 def parse(puzzle_input):
     # parse the input
-    return [line for line in puzzle_input.split()]
+    return zip([line[0] for line in puzzle_input.split()],[int(line[1:]) for line in puzzle_input.split()])
+
+def over_rotation_fix(num):
+    while num > 99: num -= 100
+    while num < 0: num += 100
+    return num
 
 def part1(parsed):
-    print(parsed)
-    return 0
+    position = 50
+    times_at_zero = 0
+    for rotation in parsed:
+        dir, distance = rotation
+        # plus direction
+        if dir == 'R': position = over_rotation_fix(position + distance)
+        # minus direction
+        else: position = over_rotation_fix(position - distance)
+        if position == 0: times_at_zero += 1
+    return times_at_zero
 
 def part2(parsed):
     return 0
